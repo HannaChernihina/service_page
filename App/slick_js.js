914d208b695250eca9111f98c933faf2b2237593
HannaChernihina <hanna_chernihina@ukr.net>
2019-07-22 01:31:02 +0300

    add section

--- App/slick_js.js
+++ App/slick_js.js
@@ -9,38 +9,45 @@ $(document).ready(function () {
         fade: true,
     });
 
+    $('.service_slider').slick({
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    autoplay: true ,
+                    autoplaySpeed: 3000,
+                });
 
-    function windowSize() {
-        if (window.matchMedia("(max-width: 520px)").matches) {
-            $('.service_slider').slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                // autoplay: true ,
-                autoplaySpeed: 3000,
-            });
-
-        } else if (window.matchMedia("(max-width: 768px)").matches) {
-            $('.service_slider').slick({
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                // autoplay: true ,
-                autoplaySpeed: 3000,
-            });
-
-        } else {
-            $('.service_slider').slick({
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                responsive: true,
-                // autoplay: true ,
-                autoplaySpeed: 3000,
-            });
-        }
-
-    }
-
-    $(window).load(windowSize); // при загрузке
-    $(window).resize(windowSize); // при изменении размеров
+    //
+    // function windowSize() {
+    //     if (window.matchMedia("(max-width: 520px)").matches) {
+    //         $('.service_slider').slick({
+    //             slidesToShow: 1,
+    //             slidesToScroll: 1,
+    //             // autoplay: true ,
+    //             autoplaySpeed: 3000,
+    //         });
+    //
+    //     } else if (window.matchMedia("(max-width: 768px)").matches) {
+    //         $('.service_slider').slick({
+    //             slidesToShow: 2,
+    //             slidesToScroll: 1,
+    //             // autoplay: true ,
+    //             autoplaySpeed: 3000,
+    //         });
+    //
+    //     } else {
+    //         $('.service_slider').slick({
+    //             slidesToShow: 3,
+    //             slidesToScroll: 1,
+    //             responsive: true,
+    //             // autoplay: true ,
+    //             autoplaySpeed: 3000,
+    //         });
+    //     }
+    //
+    // }
+    //
+    // $(window).load(windowSize); // при загрузке
+    // $(window).resize(windowSize); // при изменении размеров
 
 
 });
