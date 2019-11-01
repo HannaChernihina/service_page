9a73be93c047b3fdcee3a945aa45a5b15f7e1d08
HannaChernihina <hanna_chernihina@ukr.net>
2019-11-01 23:12:57 +0200

    clone old version

---
+++ App/slick_js.js
@@ -0,0 +1,48 @@
+
+$( document ).ready(function() {
+
+    $('.single-slide').slick({
+        slidesToShow: 1,
+        accessibility: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<div class="prevTop"></div>',
+        nextArrow: '<div class="nextTop"></div>',
+        fade: true,
+    });
+
+    $('.service_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true ,
+        arrows: true,
+        prevArrow: '<div class="prevServise"></div>',
+        nextArrow: '<div class="nextService"></div>',
+
+        autoplaySpeed: 2000,
+
+        responsive: [
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true ,
+                    infinite: true,
+                }
+            },
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    autoplay: true ,
+                    infinite: true,
+                }
+            }
+        ],
+
+    });
+
+    });
