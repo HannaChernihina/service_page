131d0fb3aab0c5917720cef73b9e585c7b512a3e
HannaChernihina <hanna_chernihina@ukr.net>
2019-11-03 02:07:26 +0200

    add effect

--- App/slick_js.js
+++ App/slick_js.js
@@ -54,6 +54,49 @@ $( document ).ready(function() {
         });
         return false;
     });
+
+
+
+$('.titleSlider').hide(0, function(){
+    setTimeout(function(){
+        $('.titleSlider').slideDown(800);
+    }, 500);
+});
+
+
+$('.nextTop').click(function(){
+        $('.titleSlider').hide(100, function() {
+            $('.titleSlider').slideDown(800) ;
+        });
+    });
+    $('.prevTop').click(function(){
+        $('.titleSlider').hide(100, function() {
+            $('.titleSlider').slideDown(800) ;
+        });
+    });
+
+// menu
+    $(function() {
+        $(document).on("click", ".mobile_menu_container .parent", function(e) {
+            e.preventDefault();
+            $(this).siblings("ul").addClass("loaded");
+        });
+        $(document).on("click", ".mobile_menu_container .back", function(e) {
+            e.preventDefault();
+            $(this).parent().parent().removeClass("loaded");
+        });
+        $(document).on("click", ".mobile_menu", function(e) {
+            e.preventDefault();
+            $(".mobile_menu_container").addClass("loaded");
+            $(".mobile_menu_overlay").fadeIn();
+        });
+        $(document).on("click", ".mobile_menu_overlay", function(e) {
+            $(".mobile_menu_container").removeClass("loaded");
+            $(this).fadeOut();
+        });
+    });
+
+
     });
 
 
